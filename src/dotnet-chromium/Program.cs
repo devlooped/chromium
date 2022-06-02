cebb1b2d4fd59100877c32fc8053d2ba8a6ca747
Daniel Cazzulino <daniel@cazzulino.com>
2022-06-02 01:15:14 -0300

    Automatically render page HtML as it's navigated
    Make it more obvious that the browser is actually running: unless --headless is passed in, a new browser tab page will be opened. If a URL was specified, it will be navigated and its HTML will be rendered in the console.
    
    As the tab page is navigated, the HTML will continue to be rendered.

--- src/dotnet-chromium/Program.cs
+++ src/dotnet-chromium/Program.cs
@@ -17,20 +17,29 @@ await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeL
 });
 
 var arg = args.FirstOrDefault(x => !x.StartsWith("--"));
-if (arg != null)
+var page = await browser.NewPageAsync();
+
+// NOTE: showcases how to interact with the browser as it's navigating
+page.FrameNavigated += async (sender, args) =>
 {
-    var page = await browser.NewPageAsync();
-    await page.GotoAsync(arg);
+    await page.WaitForNavigationAsync(new PageWaitForNavigationOptions
+    {
+        Timeout = 0,
+        WaitUntil = WaitUntilState.NetworkIdle
+    });
 
     // Example of how to get the page body HTML
-    //await page.GotoAsync(arg, new PageGotoOptions
-    //{
-    //    Timeout = 0,
-    //    WaitUntil = WaitUntilState.NetworkIdle
-    //});
-    //var body = await page.QuerySelectorAsync("body");
-    //var html = await body!.EvaluateAsync<string>("node => node.outerHTML");
-    //Console.WriteLine(html);
+    Console.WriteLine(await page.InnerHTMLAsync("body"));
+};
+
+if (arg != null)
+{
+    await page.GotoAsync(arg, new PageGotoOptions
+    {
+        Timeout = 0,
+        WaitUntil = WaitUntilState.NetworkIdle
+    });
+    Console.WriteLine(await page.InnerHTMLAsync("body"));
 }
 
 Console.WriteLine("Press Enter to exit.");
