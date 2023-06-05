3053f21762811d10923bf68408ff33b7660c5b60
Daniel Cazzulino <daniel@cazzulino.com>
2023-06-05 12:04:19 -0300

    Add SponsorLink to support the project

---
+++ src/CodeAnalysis/Analyzer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Devlooped.Chromium;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+class SponsorLinker : SponsorLink
+{
+    public SponsorLinker() : base("devlooped", "chromium")
+    { }
+}
